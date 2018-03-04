56c73431cc98274082821ea171d482cb6cba7605
Shivamp629 <shivamp629@gmail.com>
2018-03-04 11:00:37 -0500

    Update gopro_stream.py

--- gopro_stream.py
+++ gopro_stream.py
@@ -1,6 +1,8 @@
-# (1) Creates an instance of GoPro connection
-# (2) Streams to udp port 10000 
 from goprocam import GoProCamera
 from goprocam import constants
+
+# (1) Creates an instance of GoPro connection
 gopro = GoProCamera.GoPro()
+
+# (2) Streams to udp port 10000 
 gopro.stream("udp://127.0.0.1:10000")
